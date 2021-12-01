bae03a183841d4dfdfbf2dda931066bdfc4ca842
Artur Kovalchuk <arkovalchuk@playtika.com>
2021-12-01 16:53:33 +0200

    Init Spring Boot

---
+++ src/main/java/ua/kovalchuk/HttpProxySpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpProxySpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpProxySpringBootStarterApplication.class, args);
+    }
+
+}
